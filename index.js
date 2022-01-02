ca9e9e65e6a8092db5e95e84a7a8bc88ff123bfe
eugeniusms <eugeniusms@gmail.com>
2022-01-03 00:31:07 +0700

    Add Nodemon

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function(){
+app.get('/api', function(req, res){
     console.log('GET Request');
+    res.send({ name: 'Mario'});
+});
+
+app.post('/api', function(req, res){
+    console.log('GET Request');
+    res.send({ name: 'Mario'});
 });
 
 app.listen(process.env.port || 4000, function(){
